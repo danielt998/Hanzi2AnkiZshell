2c58c2a20fce75fe3f314c8f42bacc70d7d1e759
daniel <daniel@fearnley.net>
2024-09-07 17:35:46 +0100

    remove cruft

--- script.sh
+++ script.sh
@@ -1,13 +1,5 @@
 #!/bin/zsh
-
-echo "${BASH_VERSION}"
-
-#See https://stackoverflow.com/questions/10806357/associative-arrays-are-local-by-default - there are some bugs/scoping issues here
 declare -A the_dict
-# the_dict["lol"]='hi'
-
-# echo ${the_dict["lol"]}
-
 
 # cat cedict_short.u8| while read line; do
 cat cedict_ts.u8| while read line; do
@@ -16,26 +8,13 @@ cat cedict_ts.u8| while read line; do
     the_dict[${simp}]="${line}"
 done
 
-# for key value in ${(kv)the_dict}; do
-#     echo "$key -> $value"
-# done
-
-# echo ${(k)the_dict}
-# echo $the_dict[㐤]
-# 
-# echo ${the_dict[㐤]}
-
-
-# char="㐤"
-# echo ${the_dict[$char]}
-
 printCsv() {
-    line=$1
-    simp=$(echo $line | cut -d ' ' -f1)
-    pinyin_regex='^([^ ]*) ([^ ]*) \[([a-zA-ZÜü0-9 ]+)\] (.*)'
+    simp=""
     pinyin=""
     english=""
-    if [[ $line =~ $pinyin_regex ]]; then
+    regex='^([^ ]*) ([^ ]*) \[([a-zA-ZÜü0-9 ]+)\] (.*)'
+ 
+    if [[ $1 =~ $regex ]]; then
         simp=$match[1]
         pinyin=$match[3]
         english=$match[4]
@@ -43,8 +22,6 @@ printCsv() {
     fi 
     echo "${simp}\t${pinyin}\t${english}"
 }
-
-printCsv ${the_dict[$char]}
 
 input_text=$(tee)
 echo ${input_text} | sed -e 's/\(.\)/\1\n/g' | while read line; do
