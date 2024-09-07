88dabc3d3e410a716c1d307ad56f284ee074776a
daniel <daniel@fearnley.net>
2024-09-07 15:57:43 +0100

    initial commit

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+echo "${BASH_VERSION}"
+
+#See https://stackoverflow.com/questions/10806357/associative-arrays-are-local-by-default - there are some bugs/scoping issues here
+declare -A the_dict
+the_dict["lol"]='hi'
+
+echo ${the_dict["lol"]}
+
+
+# cat cedict_short.u8| while read line; do
+cat cedict_ts.u8| while read line; do
+
+    if [[ ${line:1} == '#' ]]; then continue; fi
+    simp=$(echo $line | cut -d ' ' -f1)
+    the_dict[${simp}]="${line}"
+done
+
+# for key value in ${(kv)the_dict}; do
+#     echo "$key -> $value"
+# done
+
+# echo ${(k)the_dict}
+echo $the_dict[㐤]
+# 
+# echo ${the_dict[㐤]}
+
+
+char="㐤"
+echo ${the_dict[$char]}
+
+ 
+input_text=$(tee)
+echo ${input_text} | sed -e 's/\(.\)/\1\n/g' | while read line; do
+    echo the_dict[$line]
+done
